27ac3e884fa1efa3c22e703f914f59cae8ef9e26
Oleksii <vasin.oleksii@gmail.com>
2023-10-06 12:23:20 +0200

    finished layout create

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,12 +1,13 @@
 import "./index.sass";
 import illustrationHero from "../../assets/images/illustration-hero.svg";
+import iconTarif from "../../assets/images/icon-music.svg";
 
 function Cart() {
   return (
     <div className="App">
       <div className="wrapper">
         <div className="image">
-          <img src={illustrationHero} alt="Опис зображення" />
+          <img src={illustrationHero} alt="photo girl" />
         </div>
         <div className="block-text">
           <h2>Order Summary</h2>
@@ -15,11 +16,18 @@ function Cart() {
             any device anywhere you like!
           </p>
           <div className="tarif">
-            <h3>Annual Plan</h3>
-            <span>$59.99/year</span>
-            <span className="step-change">Change</span>
+            <div className="icon">
+              <img src={iconTarif} />
+            </div>
+            <div className="plan">
+              <h3>Annual Plan</h3>
+              <span>$59.99/year</span>
+            </div>
+            <a className="step-change" href="#">
+              Change
+            </a>
           </div>
-          <div className="Buttons">
+          <div className="buttons">
             <button className="step-next">Proceed to Payment</button>
             <button className="step-back">Cancel Order</button>
           </div>
