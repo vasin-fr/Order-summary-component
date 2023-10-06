582754fc16ab32bcbd3b9645a9140945db96b99d
Oleksii <vasin.oleksii@gmail.com>
2023-10-06 12:30:43 +0200

    fixed .sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Cart from "./components/Cart";
-import "./index.sass";
+import "./_index.sass";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
